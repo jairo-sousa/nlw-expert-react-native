be245228c96b03799055e8e91ad613eaf1ae494a
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-16 23:07:27 -0300

    feat: extend Inter in tailwind.config

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,9 @@ import tw from "twrnc";
 export default function Home() {
     return (
         <View style={tw`flex-1 pt-8`}>
-            <Text style={tw`text-white text-2xl`}>Hello World!</Text>
+            <Text style={tw`text-white text-2xl font-heading`}>
+                Hello World!
+            </Text>
         </View>
     );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import("twrnc").config} */
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: "Inter_600SemiBold",
+                subtitle: "Inter_500Medium",
+                body: "Inter_400Regular",
+                bold: "Inter_700Bold",
+            },
+        },
+    },
+};
