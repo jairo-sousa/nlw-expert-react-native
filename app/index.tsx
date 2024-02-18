06e754dee8f32454ae3a9f03b331651d3825342b
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-18 00:32:07 -0300

    feat: handle category selection

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,19 @@ import tw from "twrnc";
 
 import Header from "@/components/header";
 import CategoryButton from "@/components/category-button";
+import { useState } from "react";
 
 export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0]);
+
+    function handleCategorySelect(selectedCategory: string) {
+        if (selectedCategory != category) {
+            setCategory(selectedCategory);
+        } else {
+            setCategory("");
+        }
+    }
+
     return (
         <View style={tw`flex-1 pt-8`}>
             <Header title="Faça seu pedido" cartQuantityItems={1} />
@@ -14,7 +25,13 @@ export default function Home() {
             <FlatList
                 data={CATEGORIES}
                 keyExtractor={(item) => item}
-                renderItem={({ item }) => <CategoryButton title={item} />}
+                renderItem={({ item }) => (
+                    <CategoryButton
+                        title={item}
+                        isSelectet={item === category}
+                        onPress={() => handleCategorySelect(item)}
+                    />
+                )}
                 horizontal
                 style={tw`max-h-10 mt-5`}
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 12 }}
