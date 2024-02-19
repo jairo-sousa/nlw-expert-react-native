b04b6b7a912e4b2621d4d4f80a9d96826723e0a2
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-19 15:01:42 -0300

    feat: show product cover and price

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,15 +1,28 @@
 import { styles } from "@@/app/_layout";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import tw from "twrnc";
 
 import { useLocalSearchParams } from "expo-router";
+import { PRODUCTS } from "@/utils/data/products";
 
 export default function Product() {
     const { id } = useLocalSearchParams();
 
+    const product = PRODUCTS.filter((item) => item.id == id)[0];
+
     return (
-        <View style={tw`flex-1 items-center justify-center`}>
-            <Text style={tw`text-white text-4xl`}>Product id: {id}</Text>
+        <View style={tw`flex-1`}>
+            <Image
+                source={product.cover}
+                resizeMode="cover"
+                style={tw`w-full h-52`}
+            />
+
+            <View style={tw`p-5 mt-8 flex-1`}>
+                <Text style={[tw`text-lime-400 text-2xl ml-2`, styles.heading]}>
+                    {product.price}
+                </Text>
+            </View>
         </View>
     );
 }
