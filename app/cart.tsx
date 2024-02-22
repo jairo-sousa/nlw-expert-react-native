436c8ba598f6490e4c43baa4a75fb8110b9a29fb
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-22 12:07:54 -0300

    feat: make cart products scrollable

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,6 +1,6 @@
 import tw from "twrnc";
 import { styles } from "./_layout";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 import { Header } from "@/components/header";
 import { Product } from "@/components/product";
@@ -22,27 +22,32 @@ export default function Cart() {
         <View style={tw` flex-1 pt-8`}>
             <Header title="Seu carrinho" />
 
-            {cartStore.products.length > 0 ? (
-                <View style={tw`p-5 flex-1 `}>
-                    {cartStore.products.map((product) => (
-                        <Product key={product.id} data={product} />
-                    ))}
-                </View>
-            ) : (
-                <Text
-                    style={[styles.body, tw`text-slate-400 text-center my-8`]}>
-                    Seu carrinho está vazio
-                </Text>
-            )}
+            <ScrollView>
+                {cartStore.products.length > 0 ? (
+                    <View style={tw`p-5 flex-1 `}>
+                        {cartStore.products.map((product) => (
+                            <Product key={product.id} data={product} />
+                        ))}
+                    </View>
+                ) : (
+                    <Text
+                        style={[
+                            styles.body,
+                            tw`text-slate-400 text-center my-8`,
+                        ]}>
+                        Seu carrinho está vazio
+                    </Text>
+                )}
 
-            <View style={tw`flex-row gap-2 items-center mt-5 mb-4`}>
-                <Text style={[styles.subtitle, tw`text-white text-xl`]}>
-                    Total:
-                </Text>
-                <Text style={[styles.heading, tw`text-lime-400 text-2xl`]}>
-                    {total}
-                </Text>
-            </View>
+                <View style={tw`flex-row gap-2 items-center mt-5 mb-4`}>
+                    <Text style={[styles.subtitle, tw`text-white text-xl`]}>
+                        Total:
+                    </Text>
+                    <Text style={[styles.heading, tw`text-lime-400 text-2xl`]}>
+                        {total}
+                    </Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
