b9d467012ff375041695a1ceec76da2dc7b450d2
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-22 11:54:37 -0300

    feat: show empty cart msg

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,6 +1,6 @@
 import tw from "twrnc";
 import { styles } from "./_layout";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 import { Header } from "@/components/header";
 import { Product } from "@/components/product";
@@ -14,11 +14,18 @@ export default function Cart() {
         <View style={tw` flex-1 pt-8`}>
             <Header title="Seu carrinho" />
 
-            <View style={tw`p-5 flex-1 `}>
-                {cartStore.products.map((product) => (
-                    <Product key={product.id} data={product} />
-                ))}
-            </View>
+            {cartStore.products.length > 0 ? (
+                <View style={tw`p-5 flex-1 `}>
+                    {cartStore.products.map((product) => (
+                        <Product key={product.id} data={product} />
+                    ))}
+                </View>
+            ) : (
+                <Text
+                    style={[styles.body, tw`text-slate-400 text-center my-8`]}>
+                    Seu carrinho está vazio
+                </Text>
+            )}
         </View>
     );
 }
